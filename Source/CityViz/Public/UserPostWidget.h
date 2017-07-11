6312eae3a6fee19ca9aef1d32fbaaebb07caeeae
serm tape <serm.thm@gmail.com>
2017-07-11 22:02:36 +0700

    Create UserPostWidget

---
+++ Source/CityViz/Public/UserPostWidget.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Image.h"
+#include "Blueprint/UserWidget.h"
+#include "UserPostWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CITYVIZ_API UUserPostWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widget content")
+		FString UserName;
+
+	UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Widget content")
+		FString Message;
+
+	UPROPERTY(EditAnyWhere, BluePrintReadWrite, Category = "Widget content")
+		UImage* Image;
+	
+};
