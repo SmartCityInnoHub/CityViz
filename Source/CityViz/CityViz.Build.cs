73c88e3515570c5304b7931198d87a4d29671e09
PePo Dev <gmgame.1525@gmail.com>
2017-07-20 01:20:55 -0700

    add config for http request and json , more model kmitl

--- Source/CityViz/CityViz.Build.cs
+++ Source/CityViz/CityViz.Build.cs
@@ -8,7 +8,7 @@ public class CityViz : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "Http", "Json", "JsonUtilities" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
